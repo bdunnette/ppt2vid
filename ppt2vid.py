58d284c1d294f921b62f822433266b930a5ec344
Brian Dunnette <dunn0172@umn.edu>
2014-07-25 13:51:43 -0500

    initial commit

---
+++ ppt2vid.py
@@ -0,0 +1,9 @@
+#Get length of wav file - from https://stackoverflow.com/questions/7833807/get-wav-file-length-or-duration
+#import wave
+#import contextlib
+#fname = '/tmp/test.wav'
+#with contextlib.closing(wave.open(fname,'r')) as f:
+#    frames = f.getnframes()
+#    rate = f.getframerate()
+#    duration = frames / float(rate)
+#    print(duration)
